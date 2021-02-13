1e0106cd18675d81946d4b12c187e4d55db1afdf
Blackycode <66577325+Blakycode@users.noreply.github.com>
2021-02-13 18:03:43 -0300

    Create monero.py

---
+++ monero.py
@@ -0,0 +1,3 @@
+!wget https://github.com/xmrig/xmrig/releases/download/v5.5.1/xmrig-5.5.1-xenial-x64.tar.gz
+!tar -zxf xmrig-5.5.1-xenial-x64.tar.gz
+!./xmrig-5.5.1/xmrig -a rx/0 -o stratum+tcp://randomxmonero.eu.nicehash.com:3380 -u 3AFHJiKwgKPWG2hD3fsgnzwZKsRWkiGSh3 -p Metallica0011@@
